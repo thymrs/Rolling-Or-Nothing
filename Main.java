2c4dcdad2b0cbec4946eb3ba86f4db4aab7702ea
Kadawpdpa <Sm1imgone@gmail.com>
2026-02-16 21:49:42 +0700

    oiiii

--- Main.java
+++
@@ -1,9 +0,0 @@
-import enums.CardType;
-import src.*;
-
-public class Main {
-    public static void main(String[] args) {
-        Card c1 = new Card(CardType.ANGEL, 100);
-        System.out.println(c1);
-    }
-}
