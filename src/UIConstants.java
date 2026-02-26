a631a6ba4bd2ef048c018c4a8dd4795f558880e4
PanumasS <panumassuthi@gmail.com>
2026-02-26 11:48:13 +0700

    UIConstance

--- src/UIConstants.java
+++ src/UIConstants.java
@@ -1,4 +1,20 @@
+import java.awt.Color;
 
 public class UIConstants {
 
+    // Backgrounds
+    public static final Color PRIMARY_DARK  = new Color(0x1E1E1E);
+    public static final Color PRIMARY_LIGHT = new Color(0x2A2A2A);
+
+    // Text
+    public static final Color TEXT_PRIMARY   = new Color(0xEDEDED);
+    public static final Color TEXT_SECONDARY = new Color(0xBDBDBD);
+
+    // Accents (info/success/warn/danger)
+    public static final Color ACCENT_INFO    = new Color(0x4DA3FF);
+    public static final Color ACCENT_SUCCESS = new Color(0x2ECC71);
+    public static final Color ACCENT_WARNING = new Color(0xF1C40F);
+    public static final Color ACCENT_DANGER  = new Color(0xE74C3C);
+
+    private UIConstants() {} // กันเผลอ new
 }
